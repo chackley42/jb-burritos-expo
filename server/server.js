110c84bf4a48fcdb2618b71e7e09489bd965d811
chackley42 <chackley42@gmail.com>
2023-10-25 15:53:03 -0600

    add account backend (still needs organization)

--- server/server.js
+++ server/server.js
@@ -1,11 +1,29 @@
 require('dotenv').config(); // Load environment variables from config.env
 const express = require('express');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require('./models/users.ts')
+const mongoose = require("mongoose");
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
 const routes = require('./routes/routes.js');
+const bodyParser = require('body-parser');
+
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(bodyParser.json(), urlencodedParser);
+
+const dbURI = process.env.mongoDbAtlasUri;
+
+mongoose.connect(dbURI, { useNewUrlParser:true, useUnifiedTopology:true })
+.then((res) => {
+  app.listen(8082, () => console.log("Server is live!!!!!!"))
+  
+})
+.catch(err => console.log(err))
 
 app.use(cors());
 app.use(express.json());
@@ -36,7 +54,98 @@ async function checkDatabaseExistence() {
   }
 }
 
+function verifyJWT(req, res, next) {
+  const token = req.headers["x-access-token"]?.split(' ')[1]
+
+  if (token) {
+    jwt.verify(token, process.env.PASSPORTSECRET, (err, decoded) => {
+      if (err) return res.json({
+        isLoggedIn: false,
+        message: "Failed to Authenticate"
+      })
+      req.user = {};
+      req.user.id = decoded.id
+      req.user.username = decoded.username
+      next()
+    })
+  } else {
+    res.json({message: "Incorrect Token Given", isLoggedIn: false})
+  }
+}
+
 checkDatabaseExistence();
+
+app.post("/register", async (req, res) => {
+  const user = req.body;
+
+  //check if user already exists or name has been taken by another user already
+  const takenUsername = await User.findOne({username: user.username})
+  const takenEmail = await User.findOne({email: user.email})
+
+  if (takenUsername || takenEmail) {
+    res.json({message: "Username or email has already been taken"})
+  } else{
+    user.password = await bcrypt.hash(req.body.password, 10)
+
+    const dbUser = new User({
+      username: user.username.toLowerCase(),
+      email: user.email.toLowerCase(),
+      password: user.password
+    })
+
+    await dbUser.save()
+    res.json({message: "Success"})
+  }
+})
+
+
+app.post("/login", (req, res) => {
+  const userLoggingIn = req.body;
+
+  User.findOne({ username: userLoggingIn.username })
+  .then((dbUser) => {
+    if (!dbUser) {
+      return res.json({
+        message: "Invalid Username or Password"
+      });
+    }
+
+    bcrypt.compare(userLoggingIn.password, dbUser.password)
+      .then((isCorrect) => {
+        if (isCorrect) {
+          const payload = {
+            id: dbUser._id,
+            username: dbUser.username,
+          };
+
+          jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            { expiresIn: 86400 },
+            (err, token) => {
+              if (err) return res.json({ message: err });
+              return res.json({
+                message: "Success",
+                token: "Bearer " + token
+              });
+            }
+          );
+        } else {
+          return res.json({
+            message: "Invalid Username or Password"
+          });
+        }
+      });
+  })
+  .catch((err) => {
+    return res.json({ message: err });
+  });
+});
+
+
+app.get("/getUsername", verifyJWT, (req, res) => {
+  res.json({isLoggedIn: true, username: req.user.username})
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
