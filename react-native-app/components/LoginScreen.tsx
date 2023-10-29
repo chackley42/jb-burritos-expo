ca55ccd8865d50007eef9f76ca8d73e46ac32db5
chackley42 <chackley42@gmail.com>
2023-10-29 16:53:35 -0600

    connect backend login API to front end

--- react-native-app/components/LoginScreen.tsx
+++ react-native-app/components/LoginScreen.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, Button, TouchableOpacity, StyleSheet } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import iosLocalHost from '../utils/testingConsts';
 
 interface LoginFormValues {
   username: string;
@@ -12,6 +14,9 @@ const LoginScreen = () => {
     password: '',
   });
 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState('');
+
   const handleUsernameChange = (text: string) => {
     setLoginData({ ...loginData, username: text });
   };
@@ -20,43 +25,113 @@ const LoginScreen = () => {
     setLoginData({ ...loginData, password: text });
   };
 
-  const handleLogin = () => {
-    // Handle login logic here, e.g., validate credentials
-    console.log('Username:', loginData.username);
-    console.log('Password:', loginData.password);
+  useEffect(() => {
+    // Check if user is already logged in (token exists in AsyncStorage)
+    checkLoggedInStatus();
+  }, []);
+
+  const checkLoggedInStatus = async () => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      // Token found, user is logged in
+      try {
+        // Fetch user details using the token and update the state with the username
+        const response = await fetch(`${iosLocalHost}:8080/api/getUsername`, {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${token}`
+          },
+        });
+        const userData = await response.json();
+        setUsername(userData.username);
+      } catch (error) {
+        // Handle error while fetching user details
+        console.error('Error fetching user details:', error);
+      }
+    } else {
+      // Token not found, user is not logged in
+      setIsLoggedIn(false);
+    }
   };
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.label}>Username:</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter your username"
-        value={loginData.username}
-        onChangeText={handleUsernameChange}
-      />
+  const handleLogin = async () => {
+    try {
+      const response = await fetch(`${iosLocalHost}:8080/api/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(loginData),
+      });
+  
+      const data = await response.json();
+  
+      if (response.ok) {
+        // Save JWT token to AsyncStorage
+        await AsyncStorage.setItem('token', data.token);
+        // Update login status and username
+        setIsLoggedIn(true);
+        setUsername(loginData.username);
+        // Handle successful login logic (navigate to home screen, etc.)
+        console.log('Login successful!');
+      } else {
+        // Handle login failure, show error message to the user
+        console.log('Login failed:', data.message);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+  
 
-      <Text style={styles.label}>Password:</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter your password"
-        value={loginData.password}
-        onChangeText={handlePasswordChange}
-        secureTextEntry
-      />
+  const handleLogout = async () => {
+    // Clear token from AsyncStorage and update login status
+    await AsyncStorage.removeItem('token');
+    setIsLoggedIn(false);
+  };
 
-      <Button title="Login" onPress={handleLogin} />
 
-      <TouchableOpacity style={styles.signupButton}>
-        <Text style={styles.signupText}>Create Account</Text>
-      </TouchableOpacity>
-
-      <Text style={styles.orText}>Or sign in with:</Text>
-
-      {/* Placeholder for social login buttons */}
-      {/* Add buttons for Google, Facebook, Apple login */}
-    </View>
-  );
+  if (isLoggedIn) {
+    // User is logged in, show username and logout button
+    return (
+      <View style={styles.container}>
+        <Text>Welcome, {loginData.username}!</Text>
+        <Button title="Logout" onPress={handleLogout} />
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.label}>Username:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your username"
+          value={loginData.username}
+          onChangeText={handleUsernameChange}
+        />
+  
+        <Text style={styles.label}>Password:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your password"
+          value={loginData.password}
+          onChangeText={handlePasswordChange}
+          secureTextEntry
+        />
+  
+        <Button title="Login" onPress={handleLogin} />
+  
+        <TouchableOpacity style={styles.signupButton}>
+          <Text style={styles.signupText}>Create Account</Text>
+        </TouchableOpacity>
+  
+        <Text style={styles.orText}>Or sign in with:</Text>
+  
+        {/* Placeholder for social login buttons */}
+        {/* Add buttons for Google, Facebook, Apple login */}
+      </View>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
