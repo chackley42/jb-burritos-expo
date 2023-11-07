dc1da860963c3118200b54e788e034d469680a0e
Kenny Encinas <kennyencinas@Kennys-MacBook.local>
2023-11-07 00:25:06 -0700

    adjusted the map truck view styles to display properly

--- react-native-app/app/(tabs)/home/index.tsx
+++ react-native-app/app/(tabs)/home/index.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
-import { View, Text, Pressable, StyleSheet } from 'react-native'; // Import StyleSheet
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 import { Link } from 'expo-router';
 import TruckMapView from '../../../components/TruckMapView';
 
 const Home = () => {
   return (
     <View style={styles.container}>
-      <View>
+      <View style={styles.mapContainer}>
         <Text>Food Truck's Current Location</Text>
-        {/* <TruckMapView></TruckMapView> */}
-        <Link href="/home/navigate" style={styles.link}>
-        <Text style={styles.linkText}>Navigate</Text>
-      </Link>
-      <Link href="/home/order" style={styles.link}>
-        <Text style={styles.linkText}>Place an Order</Text>
-      </Link>
-      <Link href="/home/cater" style={styles.link}>
-        <Text style={styles.linkText}>Cater</Text>
-      </Link>
+        <View style={styles.map}>
+          {/* <TruckMapView /> */}
+        </View>
       </View>
-      
+      <View style={styles.linksContainer}>
+        <View style={styles.link}>
+          <Link href="/home/navigate">
+            <Text style={styles.linkText}>Navigate</Text>
+          </Link>
+        </View>
+        <View style={styles.link}>
+          <Link href="/home/order">
+            <Text style={styles.linkText}>Place an Order</Text>
+          </Link>
+        </View>
+        <View style={styles.link}>
+          <Link href="/home/cater">
+            <Text style={styles.linkText}>Cater</Text>
+          </Link>
+        </View>
+      </View>
     </View>
   );
 };
@@ -27,19 +36,36 @@ const Home = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-around',
+    justifyContent: 'center',
     alignItems: 'center',
+  },
+  mapContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  map: {
+    width: '80%',
+    height: '60%',
+    aspectRatio: 1,
+  },
+  linksContainer: {
+    width: '100%',
   },
   link: {
     padding: 10,
     backgroundColor: '#515D52',
     borderRadius: 8,
     marginBottom: 10,
+    width: '60%', // Limit the button width to 80% of the container
+    alignSelf: 'center', // Center the button horizontally
+    justifyContent: 'center', // Center the button vertically
   },
   linkText: {
     color: '#ffffff',
     fontSize: 18,
     fontWeight: 'bold',
+    textAlign: 'center', // Center the text inside the button
   },
 });
 
