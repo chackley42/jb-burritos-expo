eafad3768aa43a86360078033ce5c7764caced33
Kenny Encinas <kennyencinas@Kennys-MacBook.local>
2023-12-07 17:51:59 -0700

    about us page

--- react-native-app/app/(tabs)/profile/AboutUs.tsx
+++ react-native-app/app/(tabs)/profile/AboutUs.tsx
@@ -1,23 +1,47 @@
-import { Text, View, ScrollView, StyleSheet, Image} from 'react-native'
-import React, { Component } from 'react'
-import LoginScreen from '../../../components/LoginScreen'
-import RegistrationScreen from './RegistrationScreen'
-import { Link, Stack } from 'expo-router';
+import { Text, View, ScrollView, StyleSheet, Image } from 'react-native';
+import React, { Component } from 'react';
+import { Stack } from 'expo-router';
 
 export class AboutUs extends Component {
   render() {
     return (
-        <ScrollView contentContainerStyle={styles.container}>
+      <ScrollView contentContainerStyle={styles.container}>
         <Stack.Screen options={{ title: 'About Us', headerStyle: { backgroundColor: '#F8E435' } }} />
-        <Text>2023 JB Burritos</Text>
+        <Image source={require('../../../assets/logo.png')} style={styles.logo} />
+        <Text style={styles.text}>&copy;2023 JB Burritos</Text>
+        <Text style={styles.description}>
+          Welcome to JB Burritos, the ultimate destination for mouthwatering breakfast burritos on wheels!
+          Our food truck is dedicated to serving you the most delicious and satisfying breakfast options,
+          crafted with love and premium ingredients. Whether you're a local looking for a quick morning bite
+          or a visitor eager to experience the flavors of JB, our burritos are sure to tantalize your taste buds.
+          Join us on a culinary journey where every bite tells a story of flavor, freshness, and pure breakfast bliss!
+        </Text>
       </ScrollView>
-    )
+    );
   }
 }
+
 const styles = StyleSheet.create({
-    container: {
-        padding: 20,
-      },
-})
+  container: {
+    flex: 1,
+    padding: 20,
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+  },
+  logo: {
+    width: 300,
+    height: 300,
+    marginBottom: 10,
+  },
+  description: {
+    textAlign: 'center',
+    fontSize: 18,
+    marginTop: 20,
+    lineHeight: 24,
+    color: '#555',
+  },
+});
 
 export default AboutUs;
